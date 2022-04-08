3de54fb345bf25208f8cd062a09ca70b2e9b9f86
jen <sjenwork@gmail.com>
2022-04-08 12:10:35 +0800

    update explosive_web src

--- explosive_web/src/components/searchbar.vue
+++ explosive_web/src/components/searchbar.vue
@@ -31,24 +31,23 @@
             :show-labels="false"
             @search-change="asyncFind"
           >
-            <template slot="tag" slot-scope="{ option, remove }"
-              ><span class="custom__tag"
-                ><span>{{ option.name }}</span
-                ><span class="custom__remove" @click="remove(option)"
-                  >❌</span
-                ></span
-              ></template
-            >
+            <template slot="tag" slot-scope="{ option, remove }">
+              <span class="custom__tag">
+                <span>{{ option.name }}</span>
+                <span class="custom__remove" @click="remove(option)">❌</span>
+              </span>
+            </template>
             <template slot="clear" slot-scope="props">
               <div
                 class="multiselect__clear"
                 v-if="selectedChem.length"
                 @mousedown.prevent.stop="clearAll(props.search)"
-              ></div> </template
-            ><span slot="noResult"
-              >Oops! No elements found. Consider changing the search
-              query.</span
-            >
+              ></div>
+            </template>
+            <span slot="noResult">
+              Oops! No elements found. Consider changing the search
+              query.
+            </span>
           </multiselect>
         </div>
       </div>
@@ -211,7 +210,7 @@ watch(
   }
 );
 
-onMounted(() => {});
+onMounted(() => { });
 </script>
 
 <style scoped>
