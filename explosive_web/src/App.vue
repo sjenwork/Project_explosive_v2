b2eabf6a1af5eb8b3c4fd214fced8e7af32d644b
jen <sjenwork@gmail.com>
2022-04-08 11:23:11 +0800

    update source code

--- explosive_web/src/App.vue
+++ explosive_web/src/App.vue
@@ -1,13 +1,53 @@
+<template>
+  <div>
+    <backgroundmap> </backgroundmap>
+    <searchbar @item="searchitems" :selectedtime="selectedtime"> </searchbar>
+    <slideController
+      :data="data"
+      :time="selectedtime"
+      :chem="selectedchem"
+      :operation="selectedoperation"
+    >
+    </slideController>
+    <timeController @time="handletime" :selectedtime="selectedtime">
+    </timeController>
+    <displayOnMap
+      :data="data"
+      :time="selectedtime"
+      :chem="selectedchem"
+      :operation="selectedoperation"
+    >
+    </displayOnMap>
+  </div>
+</template>
+
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import backgroundmap from "./components/backgroundMap.vue";
+import slideController from "./components/slideController.vue";
+import searchbar from "./components/searchbar.vue";
+import timeController from "./components/timeController.vue";
+import displayOnMap from "./components/displayOnMap.vue";
+import { ref } from "@vue/reactivity";
+
+// var selectedtime = ref(["2021 第一季", "最新申報"]);
+var selectedtime = ref(["最新申報", "最新申報"]);
+var selectedchem = ref("");
+var selectedoperation = ref("");
+var data = ref([]);
+
+function searchitems(item) {
+  selectedchem.value = item.selected.chem;
+  selectedoperation.value = item.selected.operation;
+  data.value = item.res;
+}
+function handletime(item) {
+  if (item.length == 1) {
+    item = [...item, ...item];
+  }
+  selectedtime.value = item.map((i) => i.label);
+}
 </script>
 
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
 
 <style>
 #app {
@@ -16,6 +56,5 @@ import HelloWorld from './components/HelloWorld.vue'
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- explosive_web/src/main.js
+++ explosive_web/src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {
+    createApp
+} from "vue";
+import App from "./App.vue";
+// import * as bootstrap from "bootstrap"; // Import js file
+import "bootstrap"; // Import js filebe
+import "bootstrap/dist/css/bootstrap.min.css"; // Import css file
+import "vue-multiselect/dist/vue-multiselect.css"; // Import css file
+import "@vueform/slider/themes/default.css"; // Import css file
 
-createApp(App).mount('#app')
+
+let app = createApp(App);
+app.mount("#app");
