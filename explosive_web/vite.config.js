2c4e30955973d662934f3e8ef43aa4254dfa1fe4
jen <sjenwork@gmail.com>
2022-04-08 11:19:18 +0800

    update vite.config.js

--- explosive_web/vite.config.js
+++ explosive_web/vite.config.js
@@ -3,5 +3,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  base: "/explosivemap/",
+  // publicDir: "/public/explosivemap/",
+  plugins: [vue()],
+  // server: {
+  //   proxy: {
+  //     '/explosiveapi': {
+  //       target: 'http://172.18.18.2:8000/explosiveapi',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/explosiveapi/, '/explosiveapi')
+  //     }
+
+  //   }
+  // }
 })
